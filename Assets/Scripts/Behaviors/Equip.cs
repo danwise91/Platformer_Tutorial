7ef037114ec671a9ba701a021592e45e03c146cc
danwise91 <danielwise19918@gmail.com>
2018-01-23 12:53:02 -0500

    finish

---
+++ Assets/Scripts/Behaviors/Equip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equip : AbstractBehaviour {
+	private int _currentItem = 0;
+	private Animator animator; 
+
+    public int currentItem
+    {
+        get { return _currentItem; }
+        set
+        {
+            _currentItem = value;
+            animator.SetInteger("EquippedItem", _currentItem);
+        }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        animator = GetComponent<Animator>();
+    }
+   
+}
---
+++ Assets/Scripts/Items/PowerUpFlower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpFlower : Collectable {
+    public int itemID = 1;
+    public GameObject projectPrefab; 
+
+    protected override void OnCollect(GameObject target)
+    {
+        var equipBehaviour = target.GetComponent<Equip>();
+        if (equipBehaviour != null)
+        {
+            equipBehaviour.currentItem = itemID;
+        }
+
+        var shootBehaviour = target.GetComponent<FireProjectile>();
+        if (shootBehaviour != null)
+        {
+            shootBehaviour.projectilePrefab = projectPrefab;
+        }
+    }
+}
